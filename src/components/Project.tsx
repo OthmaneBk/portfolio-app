abde2ee81c819e83477683a26a8abf53f8464060
OthmaneBk <okebennani@gmail.com>
2025-04-17 00:35:57 +0100

    ch

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,15 +1,3 @@
-import mock04 from '../assets/images/mock04.png';
-import mock05 from '../assets/images/mock05.png';
-import mock06 from '../assets/images/mock06.png';
-import mock07 from '../assets/images/mock07.png';
-import mock08 from '../assets/images/mock08.png';
-import mock09 from '../assets/images/mock09.png';
-import mock10 from '../assets/images/mock10.png';
-import Rec from '../assets/images/Rec.png';
-import PopularName from '../assets/images/PopularName.png';
-import logs from '../assets/images/logs.png';
-import Colle from '../assets/images/Colle.png';
-
 import '../assets/styles/Project.scss';
 import projects from '../data/data';
 
