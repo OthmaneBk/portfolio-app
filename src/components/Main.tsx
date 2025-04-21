8ccd299682322e650cbf3b2b01814bafdca85456
OthmaneBk <okebennani@gmail.com>
2025-04-21 09:37:05 +0100

    change texte profile

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -21,7 +21,7 @@ function Main() {
             <a href="https://ma.linkedin.com/in/othmane-kella-bennani-938142294/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
           <h1>Kella Bennani Othmane</h1>
-          <p>Passionné par le Web Scrapping, la science des données et l'intelligence artificielle, je transforme les données brutes en insights stratégiques qui créent de la valeur. </p>
+          <p>Passionné par l'écosystème des données et de l'intelligence artificielle, je maîtrise le cadre OSEMN - de l'extraction des données brutes à leur transformation en insights actionnables, en appliquant le machine learning et le deep learning pour résoudre des problèmes complexes.</p>
       
           <div className="mobile_social_icons">
             <a href="https://github.com/OthmaneBk" target="_blank" rel="noreferrer"><GitHubIcon/></a>
