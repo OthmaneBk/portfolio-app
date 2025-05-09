187c8f49a4bb1645665b9fbf5325ca44b02509d8
OthmaneBk <okebennani@gmail.com>
2025-05-09 12:09:13 +0100

    update text

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -21,7 +21,7 @@ function Main() {
             <a href="https://ma.linkedin.com/in/othmane-kella-bennani-938142294/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
           <h1>Kella Bennani Othmane</h1>
-          <p>Passionné par l'écosystème des données et de l'intelligence artificielle, je maîtrise le cadre OSEMN - de l'extraction des données brutes à leur transformation en insights actionnables, en appliquant le machine learning et le deep learning pour résoudre des problèmes complexes.</p>
+          <p>Passionné par les données et l'IA, je maîtrise le cadre OSEMN pour transformer des données brutes en insights actionnables via le Machine Learning, le Deep Learning et le traitement du langage naturel (NLP).</p>
       
           <div className="mobile_social_icons">
             <a href="https://github.com/OthmaneBk" target="_blank" rel="noreferrer"><GitHubIcon/></a>
